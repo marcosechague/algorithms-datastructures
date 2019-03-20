3b5cb45bf8b427bd9062afc5aa78cf1f3686fdd9
Marcos Echague <marcosdaniel.echague.contractor@bbva.com>
2019-03-19 23:03:34 -0300

    first commit

---
+++ src/main/java/py/com/mechague/DinamicArray.java
@@ -0,0 +1,93 @@
+package py.com.mechague;
+
+/**
+ * @author Marcos Echague
+ * @since 22/02/2019
+ */
+
+public class DinamicArray<String>{
+
+    private Object[] array;
+    private int size;
+    private int capacity;
+
+    public DinamicArray(int initialCapacity){
+        capacity = initialCapacity;
+        array = new Object[initialCapacity];
+        size = 0;
+    }
+
+    public Object get(int index){ //O(1) --Most faster
+        return array[index];
+    }
+
+    public void set(int index, Object element ){ //O(1)
+        array[index] = element;
+    }
+
+    public void delete(int index){ // O(n) --n is from index to length of the array
+
+        if(index > size) {
+            //no action, only return
+            return;
+        }
+
+        for(int i = index +1 ; i < array.length ; i++) {
+            array[i-1] = array [i];
+        }
+
+        size--;
+    }
+
+    public void add(Object element){
+        if(size == capacity){
+            resize();
+        }
+        array[size] = element;
+        size++;
+    }
+
+    public void insert(int index, Object element){
+        if(index>capacity) {
+            throw new IndexOutOfBoundsException("The index is greather than the array capacity");
+        }
+        if(size == capacity){
+            resize();
+        }
+        for(int i= size; i>=index; i--){
+            array[i] = array[i-1];
+        }
+        array[index] = element;
+    }
+
+    public void resize(){
+        int newSize = size *2;
+        Object[] arrayResized = new Object[newSize];
+        for(int i=0;i<array.length ; i++){
+            arrayResized[i] = array[i];
+        }
+        capacity = newSize;
+        array = arrayResized;
+    }
+
+    public int size(){
+        return this.size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public boolean contains(String element){
+        if(element==null){
+            return false;
+        }
+        for (int i = 0 ; i < size; i++){
+            if (element.equals(array[i])){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ src/test/java/py/com/mechague/test/DinamycArrayTest.java
@@ -0,0 +1,64 @@
+package py.com.mechague.test;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import py.com.mechague.DinamicArray;
+
+/**
+ * @author Marcos Echague
+ * @since 19/03/2019
+ */
+
+public class DinamycArrayTest {
+
+    private DinamicArray array;
+
+    @Before
+    public void setUp() {
+        array = new DinamicArray<String>(2);
+    }
+
+    @Test
+    public void getAndSet() {
+        array.set(0,"a");
+        Assert.assertEquals("a", array.get(0));
+    }
+
+    @Test
+    public void addAndInsert() {
+        array.add("a");
+        array.add("b");
+        array.add("d");
+
+        array.insert(2,"c");
+
+        Assert.assertEquals("a", array.get(0));
+        Assert.assertEquals("b", array.get(1));
+        Assert.assertEquals("c", array.get(2));
+        Assert.assertEquals("d", array.get(3));
+
+    }
+
+    @Test
+    public void delete() {
+        array.add("a");
+        array.add("b");
+        array.add("c");
+        Assert.assertEquals(3, array.size());
+
+        array.delete(1);
+        Assert.assertEquals("c", array.get(1));
+        Assert.assertEquals(2, array.size());
+    }
+
+    @Test
+    public void contains(){
+        array.add("a");
+        array.add("b");
+        array.add("c");
+        Assert.assertEquals(false, array.contains("d"));
+        Assert.assertEquals(true, array.contains("a"));
+
+    }
+}
